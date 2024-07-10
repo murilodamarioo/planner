12ac229f2ea24964a48270c6ead57c05cd99a762
Murilo Damario <murilo.gcdamario@gmail.com>
2024-07-09 23:53:32 -0300

    Implement fucntion for add new emails to invite

--- frontend-planner/src/app.tsx
+++ frontend-planner/src/app.tsx
@@ -1,9 +1,10 @@
 import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus } from 'lucide-react'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
 export function App() {
   const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false)
+  const [emailsToInvite, setEmailsToInvite] = useState<string[]>([])
 
   function openGuestsInput() {
     setIsGuestsInputOpen(true)
@@ -19,6 +20,19 @@ export function App() {
 
   function closeGuestsModal() {
     setIsGuestsModalOpen(false)
+  }
+
+  function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const data = new FormData(event.currentTarget)
+    const email = data.get('email')?.toString()
+
+    if (!email) return
+
+    setEmailsToInvite([...emailsToInvite, email])
+    
+    event.currentTarget.reset()
   }
 
   return (
@@ -97,44 +111,33 @@ export function App() {
                 <p className="text-sm text-zinc-400">Os convidados irão receber e-mails para confirmar a participação na viagem</p>
               </div>
               <div className="flex flex-wrap gap-2">
-                <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
-                  <span className="text-zinc-300">jessica.white44@yahoo.com</span>
-                  <button type="button">
-                    <X className="size-4 text-zinc-400"/>
-                  </button>
-                </div>
-
-                <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
-                  <span className="text-zinc-300">jessica.white44@yahoo.com</span>
-                  <button type="button">
-                    <X className="size-4 text-zinc-400"/>
-                  </button>
-                </div>
-
-
-                <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
-                  <span className="text-zinc-300">jessica.white44@yahoo.com</span>
-                  <button type="button">
-                    <X className="size-4 text-zinc-400"/>
-                  </button>
-                </div>
-
-                <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
-                  <span className="text-zinc-300">jessica.white44@yahoo.com</span>
-                  <button type="button">
-                    <X className="size-4 text-zinc-400"/>
-                  </button>
-                </div>
+                {
+                  emailsToInvite.map(email => {
+                    return (
+                      <div key={email} className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                        <span className="text-zinc-300">{email}</span>
+                        <button type="button">
+                          <X className="size-4 text-zinc-400"/>
+                        </button>
+                      </div>
+                    )
+                  })
+                }
               </div>
 
               <div className="w-full h-px bg-zinc-800"></div>
 
-              <form className="p-2.5 bg-zinc-950 border-zinc-850 rounded-lg flex items-center gap-2">
+              <form onSubmit={addNewEmailToInvite} className="p-2.5 bg-zinc-950 border-zinc-850 rounded-lg flex items-center gap-2">
                 <div className="px-2 flex items-center flex-1 gap-2">
                   <AtSign className="size-5 text-zinc-400"/>
-                  <input type="text" placeholder="Digite o email do convidado" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
+                  <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="Digite o email do convidado" 
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                  />
                 </div>
-                <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
                   Convidar
                   <Plus className="size-5" />
                 </button>
