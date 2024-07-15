76f822d6fbde4bac87a6af160d3429ed1826134c
Murilo Damario <murilo.gcdamario@gmail.com>
2024-07-14 22:23:03 -0300

    create components for guests, important-links, destionation-and-date, create-activity-modal and activities

---
+++ frontend-planner/src/pages/trip-details/important-links.tsx
@@ -0,0 +1,31 @@
+import { Link2, Plus } from 'lucide-react'
+
+export function ImportantLinks() {
+  return (
+    <div className="space-y-6">
+      <h2 className="font-semibold text-xl">Links importantes</h2>
+
+      <div className="space-y-5">
+        <div className="flex items-center justify-between gap-4">
+          <div className="space-y-1.5">
+            <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+            <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">https://www.airbnb.com.br/rooms/1047000111347247248213647147</a>
+          </div>
+          <Link2 className="text-zinc-400 size-5 shrink-0"/>
+        </div>
+        <div className="flex items-center justify-between gap-4">
+          <div className="space-y-1.5">
+            <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+            <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">https://www.airbnb.com.br/rooms/1047000111347247248213647147</a>
+          </div>
+          <Link2 className="text-zinc-400 size-5 shrink-0"/>
+        </div>
+      </div>
+
+      <button className="text-zinc-200 w-full justify-center rounded-lg flex items-center gap-2 bg-zinc-800 h-11 px-5 hover:bg-zinc-700">
+        <Plus className="size-5"/>
+        Cadastrar novo link
+      </button>
+    </div>
+  )
+}
